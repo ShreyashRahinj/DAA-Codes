1f2f7d4d468678647bd57c9da927735d50138ae0
Shreyash <shreyashrahinj1@gmail.com>
2023-12-05 18:20:08 +0530

    assignment 8 done

--- Assignment8.java
+++ Assignment8.java
@@ -1,42 +1,26 @@
 public class Assignment8 {
     
-    // Function to find the minimum weight 
-    // Hamiltonian Cycle
-    static int tsp(int[][] graph, boolean[] v, int currPos, int n, int count, int cost, int ans) {
- 
-        // If last node is reached and it has a link
-        // to the starting node i.e the source then
-        // keep the minimum value out of the total cost
-        // of traversal and "ans"
-        // Finally return to check for more possible values
-        if (count == n && graph[currPos][0] > 0) {
-            ans = Math.min(ans, cost + graph[currPos][0]);
-        
+    static int tsp(int[][] graph,boolean[] v,int n,int currentPos,int cost,int count,int ans){
+
+        if(count == n && graph[currentPos][0] >0){
+            ans = Math.min(ans, cost + graph[currentPos][0]);
             return ans;
         }
- 
-        // BACKTRACKING STEP
-        // Loop to traverse the adjacency list
-        // of currPos node and increasing the count
-        // by 1 and cost by graph[currPos,i] value
-        for (int i = 0; i < n; i++) {
-            if (v[i] == false && graph[currPos][i] > 0) {
- 
-                // Mark as visited
+        for(int i=0;i<n;i++){
+            if(v[i] == false && graph[currentPos][i] >0){
                 v[i] = true;
-                ans = tsp(graph, v, i, n, count + 1,
-                          cost + graph[currPos][i], ans);
- 
-                // Mark ith node as unvisited
+
+                ans = tsp(graph, v, n, i, cost+graph[currentPos][i], count+1, ans);
+
                 v[i] = false;
             }
         }
         return ans;
-
     }
+   
+    
 
     public static void main(String[] args) {
-        // n is the number of nodes i.e. V
         int n = 4;
  
         int[][] graph = {{0, 10, 15, 20},
@@ -44,19 +28,16 @@ public class Assignment8 {
                          {15, 35, 0, 30},
                          {20, 25, 30, 0}};
  
-        // Boolean array to check if a node
-        // has been visited or not
         boolean[] v = new boolean[n];
- 
-        // Mark 0th node as visited
-        v[0] = true;
+
         int ans = Integer.MAX_VALUE;
- 
-        // Find the minimum weight Hamiltonian Cycle
-        ans = tsp(graph, v, 0, n, 1, 0, ans);
- 
-        // ans is the minimum weight Hamiltonian Cycle
+
+        v[0] = true;
+
+        ans = tsp(graph,v,n,0,0,1,ans);
+
         System.out.println(ans);
+        
     }
 
     
