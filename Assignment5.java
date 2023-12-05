975c17f362d92b7164830d933c3265392015f1a7
Shreyash <shreyashrahinj1@gmail.com>
2023-12-05 19:05:17 +0530

    0/1 dp added

--- Assignment5.java
+++ Assignment5.java
@@ -9,10 +9,23 @@ public class Assignment5 {
         else return Math.max(profit[n-1] + recSol(W-weight[n-1], weight, profit, n-1), recSol(W, weight, profit, n-1));
     }
 
+    static int dpSol(int W,int[] weight,int[] profit,int n){
+        int[] dp = new int[W+1];
+        
+        for(int i=1;i<n+1;i++){
+            for(int w=W;w>=0;w--){
+                if(weight[i-1]<=w){
+                    dp[w] = Math.max(dp[w], dp[w - weight[i-1]]+profit[i-1]);
+                }
+            }
+        }
+        return dp[W];
+    }
+
     public static void main(String[] args) {
         int profit[] = new int[] { 60, 100, 120 }; 
         int weight[] = new int[] { 10, 20, 30 }; 
-        int W = 50; 
+        int W = 40; 
         int n = profit.length; 
         System.out.println(recSol(W, weight, profit, n));
     }
